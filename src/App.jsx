602489094a6ad4d48264124411c45f57fb712941
enjistudy-maker <enjistudy@gmail.com>
2025-08-12 22:08:36 +0700

    Update navigasi dan layout di App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const sayHello = () => {
+    alert("Halo! Thankiess udah mengunjungi portofolio enjOi.");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>coding boseninn bgt</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {/* Navigasi */}
+      <nav className="navigasi clearfix">
+        <div className="logo">
+          <h2>𝜗ৎ Portofolio</h2>
+        </div>
 
-export default App
+        <div className="menu">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#project">Project & Achievements</a></li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
